94b9ca8159b9a4b889024ba37666e90242248f15
Guy McAuliffe <guy.mcauliffe@icloud.com>
2019-04-09 21:16:33 -0700

    Initial setup of React App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo-fpcc-white.png';
 import './App.css';
 
 class App extends Component {
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            FirstVoices Merge Project
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.firstvoices.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            FirstVoices
           </a>
         </header>
       </div>
